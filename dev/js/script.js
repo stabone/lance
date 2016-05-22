7f33dc22befddab1252218500a3551d8d470e096
stabone <stabone@inbox.lv>
2016-05-22 17:48:36 +0300

    Latest changes
    Added 404 page, google map in contacts and information about equipment

--- dev/js/script.js
+++ dev/js/script.js
@@ -5,8 +5,14 @@ app.config(['$routeProvider', function($routeProvider) {
         when('/', {
             templateUrl: './partials/index.html'
         }).
+        when('/equipment', {
+            templateUrl: './partials/equipment.html'
+        }).
         when('/contacts', {
             templateUrl: './partials/contacts.html'
         }).
-        otherwise({ redirectTo: '/'});
+        when('/404', {
+            templateUrl: './partials/404.html'
+        }).
+        otherwise({ redirectTo: '/404'});
 }]);
--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-var app=angular.module("app",["ngRoute"]);app.config(["$routeProvider",function(t){t.when("/",{templateUrl:"./partials/index.html"}).when("/contacts",{templateUrl:"./partials/contacts.html"}).otherwise({redirectTo:"/"})}]);
+var app=angular.module("app",["ngRoute"]);app.config(["$routeProvider",function(t){t.when("/",{templateUrl:"./partials/index.html"}).when("/equipment",{templateUrl:"./partials/equipment.html"}).when("/contacts",{templateUrl:"./partials/contacts.html"}).when("/404",{templateUrl:"./partials/404.html"}).otherwise({redirectTo:"/404"})}]);
